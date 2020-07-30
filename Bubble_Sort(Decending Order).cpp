c060da978ba395a4410cf17cd7f73b150a1ca4d7
arbainrahat <68120270+arbainrahat@users.noreply.github.com>
2020-07-30 23:25:20 +0500

    Add files via upload

---
+++ Bubble_Sort(Decending Order).cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Enter Array of 5 Numbers\n";
+	int a[5];
+	int i,j,swap;
+	for(i=0;i<5;i++){
+		cin>>a[i];
+	}
+	i=4;
+	while(i>0)
+	{
+		j=0;
+		while(j<i)
+		{
+			if(a[j]<a[j+1])
+			{
+				swap=a[j];
+				a[j]=a[j+1];
+				a[j+1]=swap;
+			}
+			j=j+1;
+		}
+		i=i-1;
+	}
+	cout<<"Bubble Sort(Decending Order)\n";
+	for(i=0;i<5;i++){
+		cout<<a[i]<<"	";
+	}
+	cin.get();
+	system("pause");
+}
